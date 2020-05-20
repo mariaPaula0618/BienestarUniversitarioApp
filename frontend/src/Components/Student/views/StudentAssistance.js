f88b6ca4ef1b68e9156b5dbaa70147b33e4dfbfd
Maria Paula Vargas C <mariapaulavargasc@MacBook-Pro-de-Maria-Paula.local>
2020-05-20 18:39:10 -0500

    import axios

--- frontend/src/Components/Student/views/StudentAssistance.js
+++ frontend/src/Components/Student/views/StudentAssistance.js
@@ -1,12 +1,22 @@
 import React, { Component } from "react";
 import Calendar from "../../Calendar/Calendar"
+import axios from 'axios';
 
 
 class StudentAssistance extends Component{
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            id_activity : props.match.params.id_activity,
+            students: [
+            ]
+        }
+  
+      }
+
     render(){
-        let id_activity= this.props.match.params.id_activity
-        console.log("Llego?")
+        
         return(<div className="bienestartActivities-container">
             <h1>Soy un gato y mi id es  {id_activity}</h1>
             <Calendar />
