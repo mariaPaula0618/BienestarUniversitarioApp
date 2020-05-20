a4142c995cf8fd2ba3026d22de1936b48b5f61ef
SamuelAbonia <samuel@pop-os.localdomain>
2020-05-20 18:40:02 -0500

    commit control

--- frontend/src/Components/Admin/views/ActivityList.js
+++ frontend/src/Components/Admin/views/ActivityList.js
@@ -1,11 +1,44 @@
 import React, { Component } from "react";
+import { Table } from 'react-bootstrap';
+import axios from 'axios';
 
 
 
 class ActivityList extends Component{
 
+    constructor(){
+        super();
+        this.state = {
+            activies: []
+        }
+    }
+
+
+    async getActivities(){
+        axios.get("http://127.0.0.1:8000/api/activity/")
+        .then((response) => {
+            
+        })
+    }
+
     render(){
         return(<div className="activitylist-container">
+             <h1> Lista de actividades</h1>
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Nombre</th>
+                        <th>Descripcion</th>
+                        <th>Profesor</th>
+                        <th>Espacio</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    
+                </tbody>
+            </Table>
+            
         </div>
 
         )
