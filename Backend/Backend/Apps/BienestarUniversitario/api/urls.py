13458f94731dc5de3924f8c35f10f9795e3a05d5
Maria Paula Vargas C <mariapaulavargasc@MacBook-Pro-de-Maria-Paula.local>
2020-05-21 10:49:13 -0500

    conflicts fixed

--- Backend/Backend/Apps/BienestarUniversitario/api/urls.py
+++ Backend/Backend/Apps/BienestarUniversitario/api/urls.py
@@ -9,11 +9,8 @@ from .views import (
     InscriptionCreateView,
     CourseCreateView,
     ProfessorActivitysAvailables,
-<<<<<<< HEAD
     ListActivitiesOfStudents,
-=======
     UserAppListByRoleView
->>>>>>> b2b0605181a9f63d7e5b871a7564f7c767e7b2fe
 )
 
 urlpatterns = [
